c1efe9f5f169b26d4fc5fcf129bde9cdeedacb4d
SMR-sassa <samrufino13@gmail.com>
2020-06-03 16:35:07 -0300

    Fixed bug

--- script/create-point.js
+++ script/create-point.js
@@ -27,11 +27,15 @@ function getCities ( event ) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${citieValue}/municipios`
 
+  citieSelect.innerHTML = "<option value>Selecione a cidade</option>"
+  citieSelect.disabled = true
+
   fetch(url)
   .then( (res) => res.json() )
   .then( cities => {
+
     for( city of cities ) {
-      citieSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citieSelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
 
     citieSelect.disabled = false
